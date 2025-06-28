e748899b8d321bb59cf2355b5883a1357b76f79b
Dan Roach <dan.roach.linux@gmail.com>
2025-06-28 18:57:45 -0400

    register_file: parameterize size based on address length, rewrite to look like memory in next commit

--- src/register_file.sv
+++ src/register_file.sv
@@ -1,32 +1,30 @@
 module register_file
-	#(parameter WIDTH=32) (
+	#(parameter XLEN=32, ADDR_WIDTH=5) (
 	input logic clk,
-	input logic clr,	// positive anynchronous clear
-	// TODO: base the widths of the _index variables on the WIDTH
-	// parameter
-	input logic [4:0] rs1_index,
-	input logic [4:0] rs2_index,
-	input logic [4:0] rd_index,
-	input logic [WIDTH - 1:0] rd,
+	input logic reset,
+	input logic [ADDR_WIDTH-1:0] rs1_index,
+	input logic [ADDR_WIDTH-1:0] rs2_index,
+	input logic [ADDR_WIDTH-1:0] rd_index,
+	input logic [XLEN-1:0] rd,
 	input logic write_en,
 
-	output logic [WIDTH - 1:0] rs1,
-	output logic [WIDTH - 1:0] rs2);
+	output logic [XLEN-1:0] rs1,
+	output logic [XLEN-1:0] rs2);
 
-	logic [WIDTH - 1:0][WIDTH - 1:0] registers;
+	reg [XLEN-1:0] registers [0:((2**ADDR_WIDTH)-1)];
 	
-	always @(posedge clk, posedge clr)
-		if (clr) begin
-			foreach (registers[i]) begin
-				registers[i] <= 'b0;
-				rs1 <= 'b0;
-				rs2 <= 'b0;
+	always @(posedge clk)
+		if (!reset) begin
+			for (i = 0; i < ((2**ADDR_WIDTH)-1); i = i + 1) begin
+				registers[i] <= 0;
 			end
-		end
-		else begin
-			rs1 <= registers[rs1_index];
-			rs2 <= registers[rs2_index];
+		end else begin
 			if (write_en)
 				registers[rd_index] <= rd;
+			// TODO add else reg[rd_ind] <= rd if no work
+			// but that seems pointless
 		end
+	
+	assign rs1 = registers[rs1_index];
+	assign rs2 = registers[rs2_index];
 endmodule

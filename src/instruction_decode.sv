569c2d0bc5c3365e09c40a158cf7849cbbcc308e
Dan Roach <dan.roach.linux@gmail.com>
2025-06-28 18:55:59 -0400

    instruction_decode: finish initial logic, add no-op testbench

--- src/instruction_decode.sv
+++ src/instruction_decode.sv
@@ -8,9 +8,10 @@ module instruction_decode #(parameter XLEN=32) (
 
 	output logic [XLEN-1:0] immediate,
 
-	output logic op1_src,	// mux input to select data source for
-				// the first opernad of the alu
-				// 0 for register value, 1 for 32'b0
+	output logic [1:0] op1_src,	// mux input to select data source for
+					// the first opernad of the alu
+					// 0 for register value, 1 for PC,
+					// 2 for 32'b0
 
 	output logic op2_src,	// mux input to select data source for
 				// the second operand of the alu
@@ -26,6 +27,7 @@ module instruction_decode #(parameter XLEN=32) (
 	// branch if (jump || (branch && (branch_if_zero ~^ zero)))
 	// in english:
 	// branch if unconditional jump or conditional and condition is met
+	// these signals go into the branch_module
 	output logic branch,		// bool to jump conditionally
 	output logic branch_if_zero,	// bool indicating the condition to jump
 	output logic jump,		// bool to jump unconditionally
@@ -45,19 +47,19 @@ module instruction_decode #(parameter XLEN=32) (
 	localparam LUI = 'b0110111;
 	localparam AUIPC = 'b0010111;
 
-	logic opcode = inst[6:0];
-	logic funct3 = inst[14:12];
+	logic [6:0] opcode = instruction[6:0];
+	logic [2:0] funct3 = instruction[14:12];
 
 	// these values always map to these bits in the instruction ... but
 	// these bits in the instruction are not always interpreted as these
 	// values
-	assign rs1 = inst[19:15];
-	assign rs2 = inst[24:20];
-	assign rd = inst[11:7];
+	assign rs1 = instruction[19:15];
+	assign rs2 = instruction[24:20];
+	assign rd = instruction[11:7];
 
 	// branch and jump signals
-	assign jump = (opcode == 'b1101111 || opcode == 'b1100111) ? 1 : 0;
-	assign branch = (opcode == 'b1100011) ? 1 : 0;
+	assign jump = (opcode == JAL || opcode == I_TYPE_JALR) ? 1 : 0;
+	assign branch = (opcode == B_TYPE) ? 1 : 0;
 	always_comb
 		case (funct3)
 			'b000,	// beq
@@ -70,7 +72,7 @@ module instruction_decode #(parameter XLEN=32) (
 
 	// ALU operation and sign
 	always_comb
-		if (opcode == 'b1100011)	// B type
+		if (opcode == B_TYPE)
 			case (funct3)
 				'b000, 'b001:	// beq and bne
 				begin
@@ -99,39 +101,69 @@ module instruction_decode #(parameter XLEN=32) (
 		else	// R type and I type, and other instruction types will not read this
 		begin
 			alu_op = funct3;
-			sign = (opcode == R_TYPE) ? inst[30] : 0;	// R type specific
+			sign = (opcode == R_TYPE) ? instruction[30] : 0;	// R type specific
 		end
 
-	// Immediate value assignment
+	// Immediate value computation and assignment
 	// Page 27
+	/* verilator lint_off WIDTHTRUNC */
+	logic [XLEN-1:0] i_type_immediate = {
+		{XLEN{instruction[31]}}, instruction[31:20]
+	};
+	logic [XLEN-1:0] b_type_immediate = {
+		{XLEN{instruction[31]}},
+		instruction[31],
+		instruction[7],
+		instruction[30:25],
+		instruction[11:8],
+		1'b0
+	};
+	logic [XLEN-1:0] s_type_immediate = {
+		{XLEN{instruction[31]}},
+		instruction[31:25],
+		instruction[11:7]
+	};
+	logic [XLEN-1:0] j_type_immediate = {
+		{XLEN{instruction[31]}},
+		instruction[20],
+		instruction[10:1],
+		instruction[11],
+		instruction[19:12],
+		1'b0
+	};
+	logic [XLEN-1:0] u_type_immediate = {
+		instruction[31:12],
+		{12{1'b0}}
+	};
+	/* verilator lint_on WIDTHTRUNC */
+
 	always_comb
 		case (opcode)
-			R_TYPE:
-				immediate = 0;
-			I_TYPE_ALU,
-			I_TYPE_LOAD,
-			I_TYPE_JALR:
-				immediate = {{XLEN{inst[31]}}, inst[31:20]};	// TODO test
+			I_TYPE_ALU, I_TYPE_LOAD, I_TYPE_JALR:
+				immediate = i_type_immediate;
 			B_TYPE:
-				immediate = {{XLEN{inst[31]}}, inst[31], inst[7], inst[30:25], inst[11:8], 1'b0};
+				immediate = b_type_immediate;
 			S_TYPE:
-				immediate = {{XLEN{inst[31]}}, inst[31:25], inst[11:7]};
-
-			JAL:
-				immediate = {{XLEN{inst[31]}}, inst[20], inst[10:1], inst[11], inst[19:12], 1'b0};
-			LUI,
-			AUIPC:
-				immediate = {inst[31:12], {12{'b0}}};
+				immediate = s_type_immediate;
+			JAL:		// J type
+				immediate = j_type_immediate;
+			LUI, AUIPC:	// U type
+				immediate = u_type_immediate;
 			default:
 				immediate = 0;
 		endcase
 
 	// ALU OP1 source
-	// This is almost always the register value, but in the case of lui,
-	// we can just use the ALU's adder to add 0 with the immediate
+	// This is almost always the register value.
+	// In the case of auipc, we pass the PC into the adder to add with the
+	// immedaiate
+	// In the case of lui, we can just use the ALU's adder to add 0 with
+	// the immediate
 	always_comb
 		case (opcode)
 			LUI:
+				op1_src = 2;
+			AUIPC:
 				op1_src = 1;
 			default:
 				op1_src = 0;
@@ -155,20 +187,7 @@ module instruction_decode #(parameter XLEN=32) (
 				op2_src = 0;
 		endcase
 
-	// B type
-	// using registers for comparison, using separate adder to
-	// compute new PC
-
-	// S type
-	// address = rs1 + imm
-	// rs2 gets stored
-
-	// TODO:
-	// seems we need an additional adder for PC + immediate, this
-	// satisfies branch computations
-	// TODO: op1_src to include PC for AUIPC, I didn't realize this stores
-	// the result in rd
-	// TODO: rf_write_en and mem_write_en
+	// Register file and memory write enable signals
 	always_comb
 		case (opcode)
 			LUI,
--- test/instruction_decode_tb.sv
+++ test/instruction_decode_tb.sv
@@ -5,7 +5,7 @@ module instruction_decode_tb();
 	logic [4:0] rd;
 
 	logic [31:0] immediate;
-	logic op1_src;
+	logic [1:0] op1_src;
 	logic op2_src;
 	logic [2:0] alu_op;
 	logic sign;
@@ -14,4 +14,19 @@ module instruction_decode_tb();
 	logic jump;
 	logic rf_write_en;
 	logic mem_write_en;
+
+	instruction_decode dut(.instruction(instruction),
+				.rs1(rs1),
+				.rs2(rs2),
+				.rd(rd),
+				.immediate(immediate),
+				.op1_src(op1_src),
+				.op2_src(op2_src),
+				.alu_op(alu_op),
+				.sign(sign),
+				.branch(branch),
+				.branch_if_zero(branch_if_zero),
+				.jump(jump),
+				.rf_write_en(rf_write_en),
+				.mem_write_en(mem_write_en));
 endmodule

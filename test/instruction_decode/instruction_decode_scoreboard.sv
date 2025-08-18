89af1141c8f00daf7234ea4ab9cfcdea5b90ef6c
Daniel Roach <roachdj@amazon.com>
2025-08-18 14:38:11 -0400

    instruction_decode: UVM progress commit - can't test at work

--- test/instruction_decode/instruction_decode_scoreboard.sv
+++ test/instruction_decode/instruction_decode_scoreboard.sv
@@ -21,13 +21,14 @@ class instruction_decode_scoreboard #(parameter XLEN=32) extends uvm_component;
 	endfunction
 
 	function void write(instruction_decode_transaction tx);
-		// TODO figure out why instruction is always X
 		if (!validate_immediate(tx.instruction, tx.immediate))
 			`uvm_error("SCOREBOARD", $sformatf("Immediate value 0x%0h mismatched expected output 0x%0h for instruction 0x%0h", tx.immediate, expected_immediate(tx.instruction), tx.instruction))
 		else
 			`uvm_info("SCOREBOARD", $sformatf("Immediate value 0x%0h matched expected output for instruction 0x%0h", tx.immediate, tx.instruction), UVM_NONE)
 
 		// TODO add validation logic for other decode signals
+		validate_branch_signals(tx);	// no need to return anything - validation done inside the function
+		validate_alu_signals(tx);
 	endfunction
 
 	function logic validate_immediate(logic[31:0] instruction, logic[XLEN-1:0] immediate);
@@ -98,31 +99,83 @@ class instruction_decode_scoreboard #(parameter XLEN=32) extends uvm_component;
 	* | + <signal2> == expected_<signal2>
 	* + ...
 	*/
-	function logic validate_branch_signals(logic[31:0] instruction);
-		// Rather than just &&ing these together, checking them
-		// individually to allow me to log the actual signal the
-		// validation failed on.
-		logic valid = 1;
-		if (!validate_jump(instruction)) begin
-			`uvm_error("SCOREBOARD", $sformatf("jump signal %d did not match the expected value %d for instruction 0x%0h", tx.jump, expected_jump(instruction), tx.instruction))
-			valid = 0;
+	function void validate_branch_signals(instruction_decode_transaction tx);
+		// expected signals
+		logic expected_jump = expected_jump(tx.instruction);
+		logic expected_branch = expected_branch(tx.instruction);
+		logic expected_branch_if_zero = expected_branch_if_zero(tx.instruction);
+		logic expected_branch_base = expected_branch_base(tx.instruction);
+
+		// validate tx.jump
+		if (tx.jump != expected_jump) begin
+			`uvm_error("SCOREBOARD", $sformatf("jump signal %d did not match the expected value %d for instruction 0x%0h", tx.jump, expected_jump, tx.instruction))
 		end
 
-		if (!validate_branch(instruction)) begin
-			`uvm_error("SCOREBOARD", $sformatf("branch signal %d did not match the expected value %d for instruction 0x%0h", tx.branch, expected_branch(instruction), tx.instruction))
-			valid = 0;
+		// validate tx.branch
+		if (tx.branch != expected_branch) begin
+			`uvm_error("SCOREBOARD", $sformatf("branch signal %d did not match the expected value %d for instruction 0x%0h", tx.branch, expected_branch, tx.instruction))
 		end
 
-		if (!validate_branch_if_zero(instruction)) begin
-			`uvm_error("SCOREBOARD", $sformatf("branch_if_zero signal %d did not match the expected value %d for instruction 0x%0h", tx.branch_if_zero, expected_branch_if_zero(instruction), tx.instruction))
-			valid = 0;
+		// validate tx.branch_if_zero
+		if (tx.branch_if_zero != expected_branch_if_zero) begin
+			`uvm_error("SCOREBOARD", $sformatf("branch_if_zero signal %d did not match the expected value %d for instruction 0x%0h", tx.branch_if_zero, expected_branch_if_zero, tx.instruction))
 		end
 
-		if (!validate_branch_base(instruction)) begin
-			`uvm_error("SCOREBOARD", $sformatf("branch_base signal %d did not match the expected value %d for instruction 0x%0h", tx.branch_base, expected_branch_base(instruction), tx.instruction))
-			valid = 0;
+		// validate tx.branch_base
+		if (tx.branch_base != expected_branch_base) begin
+			`uvm_error("SCOREBOARD", $sformatf("branch_base signal %d did not match the expected value %d for instruction 0x%0h", tx.branch_base, expected_branch_base, tx.instruction))
 		end
-
-		return valid;
 	endfunction
+
+	function logic expected_jump(logic[31:0] instruction);
+		if (instruction[6:0] == JAL || instruction[6:0] == I_TYPE_JALR)
+			return 1;
+		else
+			return 0;
+	endfunction
+
+	function logic expected_branch(logic[31:0] instruction);
+		if (instruction[6:0] == B_TYPE)
+			return 1;
+		else
+			return 0;
+	endfunction
+
+	function logic expected_branch_if_zero(logic[31:0] instruction);
+		if (instruction[14:12] inside { 'b000, 'b011, 'b111 })
+			return 1;
+		else
+			return 0;
+	endfunction
+
+	function logic expected_branch_base(logic[31:0] instruction);
+		if (instruction[6:0] == I_TYPE_JALR)
+			return 1;
+		else
+			return 0;
+	endfunction
+
+	function void validate_alu_signals(instruction_decode_transaction tx);
+		logic [2:0] expected_alu_op = expected_alu_op(tx.instruction);	// TODO args
+		logic expected_sign = expected_sign(tx.instruction);
+		logic [1:0] expected_op1_src = expected_op1_src(tx.instruction);
+		logic expected_op2_src = expected_op2_src(tx.instruction);
+
+		if (tx.alu_op != expected_alu_op) begin
+			`uvm_error("SCOREBOARD", $sformatf("alu_op signal 0x%0h did not match expected value 0x%0h for instruction 0x%0h", tx.alu_op, expected_alu_op, tx.instruction)
+		end
+
+		if (tx.sign != expected_sign) begin
+			`uvm_error("SCOREBOARD", $sformatf("sign signal 0x%0h did not match expected value 0x%0h for instruction 0x%0h", tx.sign, expected_sign, tx.instruction)
+		end
+
+		if (tx.op1_src != expected_op1_src) begin
+			`uvm_error("SCOREBOARD", $sformatf("op1_src signal 0x%0h did not match expected value 0x%0h for instruction 0x%0h", tx.op1_src, expected_op1_src, tx.instruction)
+		end
+
+		if (tx.op2_src != expected_op2_src) begin
+			`uvm_error("SCOREBOARD", $sformatf("op2_src signal 0x%0h did not match expected value 0x%0h for instruction 0x%0h", tx.op2_src, expected_op2_src, tx.instruction)
+		end
+	endfunction
+
 endclass

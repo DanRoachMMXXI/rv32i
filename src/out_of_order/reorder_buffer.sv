ec869b8d7084975297f7034da167633f4dd8b1ad
Dan Roach <dan.roach.linux@gmail.com>
2025-12-02 20:30:09 -0500

    reorder_buffer: rewrite progress commit

--- src/out_of_order/reorder_buffer.sv
+++ src/out_of_order/reorder_buffer.sv
@@ -1,12 +1,4 @@
 /*
- * This is going to need to be implemented as a circular buffer
- * In hardware, this is probably going to look like BUF_SIZE
- * registers with two log2(BUF_SIZE) bits counters.
- * The read_from counter will go into an output mux that commits
- * the instruction, and increments when an instruction commits.
- * The write_to counter will increment whenever an instruction
- * is stored in the buffer.
- *
  * TODO: be mentally prepared to handle flushing mispredicts
  * it might not be too bad if done simply: flush all instructions when the
  * branch result commits.  that would be very inefficient tho, because we want
@@ -40,123 +32,118 @@
  *
  * It may become easier to offload the flushing of the buffer to another
  * component, and take in the signals to flush specific entries of the buffer.
- *
- * TODO: go back and make sure you understand how stores are done with reorder
- * buffers
- * ^ done, written in Obsidian Vault, but effectively the address is computed
- * by (what seems to be) a dedicated address calculation functional unit (as
- * in the diagram on pg 210), then the value to be stored may be available or
- * is otherwise read from the CDB.
  */
+interface reorder_buffer_entry #(parameter XLEN);
+	logic valid;
+	logic [1:0] instruction_type;
+	logic [XLEN-1:0] destination;
+	logic [XLEN-1:0] value;
+	logic ready;
+	// eventually I'll need exceptions
+endinterface
 
 module reorder_buffer #(
 		parameter XLEN=32,
 		// For now, keep TAG_WIDTH as a parameter for syntesizability
 		// but keep it set to log2(BUF_SIZE)
-		parameter TAG_WIDTH=4,
-		parameter BUF_SIZE=16) (
+		parameter TAG_WIDTH=8,
+		parameter BUF_SIZE=64) (
 	// Synchronous input signals
 	input logic clk,
 	input logic reset,	// active low
 
 	// input signals for the instruciton to store in the buffer
-	// TODO: better names for these when brain is working better than it
-	// is at 7am.
-	input logic [4:0] rd_index_in,
-	input logic input_en,
-
-	// CDB inputs
-	input logic cdb_enable,
-	input logic [TAG_WIDTH-1:0] cdb_tag,
+	// TODO
+	input logic input_en,	// enable to read the values on the below signals
+	input logic [1:0] instruction_type_in,
+	input logic [XLEN-1:0] destination_in,
+	// I'm unsure any instructions will actually store a value straight
+	// into the ROB.  Maybe there's an argument for LUI and/or AUIPC.
+	// Since they're issued in order, nothing will be waiting for them
+	//
+	// Stores also may have the value immediately ready, but need to have
+	// their address calculated and read from the memory address bus.
+	input logic [XLEN-1:0] value_in,
+	input logic ready_in,
+	
+	// common data bus signals
+	input logic cdb_active,
 	input logic [XLEN-1:0] cdb_data,
+	input logic [TAG_WIDTH-1:0] cdb_tag,
 
-	// Data forwarding outputs
-	// These signals are the buffer
-	output reg [XLEN-1:0] [0:BUF_SIZE-1] rd_values,	// need the values to forward them
-	output reg [4:0] [0:BUF_SIZE-1] rd_indices,	// need the indices to know if the values need to be forwarded
-	output reg [0:BUF_SIZE-1] ready,	// need to know if data is ready
-	output reg [0:BUF_SIZE-1] valid,	// need to know if entry is valid
+	// memory address bus - a separate bus where the address FU sends
+	// addresses to the ROB for STORES ONLY
+	input logic memory_addr_bus_active,
+	input logic [XLEN-1:0] memory_addr_bus_data,	// the address
+	input logic [XLEN-1:0] memory_addr_bus_tag,
 
-	// Instruction commit outputs
-	// This is basically the value, index, and ready & valid of the
-	// instruction pointed to by read_from
-	// Imagine this as the outputs from the multiplexer selected by
-	// read_from
-	output logic [XLEN-1:0] instruction_commit_value,
-	output logic [4:0] instruction_commit_index,
-	output logic commit,	// ready[read_from] && valid[read_from]
-				// controls the actual committing of the
-				// instruction
-
-	output logic full	// just & all the valid bits
+	// the buffer itself
+	// assuming this will be an output since a few things will need to be
+	// read from this:
+	// - load buffer needs to see if any ROB entries have stores for
+	// a load
+	// - when instructions are issued, the ROB needs to be referenced to
+	// get the value or the tag/index that it will be broadcast to the CDB
+	// with
+	output reorder_buffer_entry [0:BUF_SIZE-1] buffer
 	);
 
-	// CIRCULAR BUFFER POINTERS
-	reg [TAG_WIDTH-1:0] read_from;
-	reg [TAG_WIDTH-1:0] write_to;
+	integer i;	// used to reset all buffer entries
 
-	// I think I could just check valid[write_to], if the entry that would
-	// be written already has something, it's full
-	assign full = &valid;
-
-	assign instruction_commit_value = rd_values[read_from];
-	assign instruction_commit_index = rd_indices[read_from];
-	assign commit = ready[read_from] && valid[read_from];	// the and with valid may be useless, but doesn't hurt
+	// circular buffer pointers
+	logic [TAG_WIDTH-1:0] read_from;
+	logic [TAG_WIDTH-1:0] write_to;
 
 	always @ (posedge clk) begin
 		if (!reset) begin
-			// reset counters
-			read_from = 0;
-			write_to = 0;
+			read_from <= 0;
+			write_to <= 0;
 
 			// reset buffer contents
-			for (int i = 0; i < BUF_SIZE; i = i + 1) begin
-				rd_values[i] = 0;
-				rd_indices[i] = 0;
-				ready[i] = 0;
-				valid[i] = 0;	// this is the important one
+			// I think the only thing that matters is valid = 0
+			for (i = 0; i < BUF_SIZE; i = i + 1) begin
+				buffer[i].valid <= 0;
+				buffer[i].instruction_type <= 0;
+				buffer[i].destination <= 0;
+				buffer[i].value <= 0;
+				buffer[i].ready <= 0;
+				// TODO reset exceptions
 			end
 		end else begin
-			// synchronous logic
-			// it might be wise to check for overflows and make
-			// assertions during simulation
+			// TODO: need to figure out how to know that both the value AND
+			// address are ready for stores, it can't just be on the event of the
+			// CDB or the memory address bus alone as they can probably come in
+			// either order.
 
-			// if an instruction is to be stored in the buffer
+			// store a new instruction in the buffer
 			if (input_en) begin
-				// store input index in write_to
-				rd_indices[write_to] = rd_index_in;
-				ready[write_to] = 0;
-				valid[write_to] = 1;
-
-				// increment write_to to point to next buffer
-				// entry
-				write_to = write_to + 1;
+				buffer[write_to].valid <= 1;
+				buffer[write_to].instruction_type <= instruction_type_in;
+				buffer[write_to].destination <= destination_in;
+				buffer[write_to].value <= value_in;
+				buffer[write_to].ready <= ready_in;
 			end
 
-			// if committing the instruction at buf[read_from]
-			if (commit) begin
-				// clear the buffer entry at index read_from
-				// pretty sure only valid needs to be cleared
-				// but clearing the other stuff for easier
-				// visibility while testing
-				rd_values[read_from] = 0;
-				rd_indices[read_from] = 0;
-				ready[read_from] = 0;
-				valid[read_from] = 0;
-
-				read_from = read_from + 1;
+			// read a value off the CDB
+			if (cdb_active) begin
+				buffer[cdb_tag].value <= cdb_data;
+				buffer[cdb_tag].ready <= 1;
 			end
 
-			if (cdb_enable) begin
-				// pretty stupid logic here but I don't see
-				// why it needs to be more complex yet
-				// TODO: maybe some sanity check assertions?
-				// check the valid bit, etc
-				rd_values[cdb_tag] = cdb_data;
-				ready[cdb_tag] = 1;
-				// in the case of an invalid tag, this could
-				// set the ready bit on an entry that has
-				// valid = 0
+			// read an address off the memory address bus
+			if (memory_addr_bus_active) begin
+				// TODO: can't just have an "active" like the CDB, it needs to be
+				// targeted for the ROB OR only update store instructions.  Load
+				// instructions use the destination field of the ROB entry for the
+				// target register, so we don't want to overwrite that with the
+				// address to be loaded - that's stored in the load buffer.
+				buffer[memory_addr_bus_tag].destination <= memory_addr_bus_data;
+			end
+
+			// instruction commit
+			if (buffer[read_from].ready) begin
+				// TODO commit logic
+				read_from <= read_from + 1;
 			end
 		end
 	end
